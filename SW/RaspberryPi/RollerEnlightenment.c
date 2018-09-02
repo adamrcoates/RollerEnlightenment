d1fa701f49805ecc00f694b05821fefed3c809e0
ARC3 <adam.r.coates@googlemail.com>
2018-09-02 16:53:14 +0000

    code compiles and runs, ant stick responds with N????

--- SW/RaspberryPi/RollerEnlightenment.c
+++ SW/RaspberryPi/RollerEnlightenment.c
@@ -59,7 +59,7 @@ void send_speed(int fd){
     unsigned char speed_LSB = (unsigned char) (speed_val - 256.0*(float)speed_MSB);;
     unsigned char HR = 0x00;
     unsigned char data_ar[] = {0x4E,0x00,0x10,elapse_time,dist_tr,speed_LSB,speed_MSB,HR,0x03};
-    send_msg(fd, , data_ar,9);
+    send_msg(fd, data_ar,9);
     
 }
 
@@ -71,14 +71,14 @@ void send_cad_pow(int fd){
     unsigned char power_MSB = (unsigned char) (power_val/256);
     unsigned char power_LSB = (unsigned char) (power_val - 256.0*(float)power_MSB);;
     unsigned char data_ar[] = {0x4E,0x00,0x15,0xFF,0xFF,0xFF,RPM,power_LSB,power_MSB,0x03};
-    send_msg(fd, , data_ar,10);
+    send_msg(fd, data_ar,10);
     
 }
 
 void send_general(int fd){
     printf("Send_general:\t\t");
     unsigned char data_ar[] = {0x4E,0x00,0x11,0xFF,0xFF,0x23,0x00,0x44,0x58,0x03};
-    send_msg(fd, , data_ar,10);
+    send_msg(fd, data_ar,10);
     
     
 }
@@ -86,40 +86,40 @@ void send_general(int fd){
 ///ANT device setup
 void setup_ant(int fd) {
   unsigned char data_ar1[] = {0x4A,0x00};
-  send_msg(fd, , data_ar1,2);
+  send_msg(fd, data_ar1,2);
   usleep(250000);
   unsigned char data_ar2[] = {0x4D,0x00,0x54};
-  send_msg(fd, , data_ar2,3);
+  send_msg(fd, data_ar2,3);
   usleep(250000);
   unsigned char data_ar3[] = {0x46,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-  send_msg(fd, , data_ar3,10);
+  send_msg(fd, data_ar3,10);
   usleep(250000);
   unsigned char data_ar4[] = {0x46,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-  send_msg(fd, , data_ar4,10);
+  send_msg(fd, data_ar4,10);
   usleep(250000);
   unsigned char data_ar5[] = {0x46,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-  send_msg(fd, , data_ar5,10);
+  send_msg(fd, data_ar5,10);
   usleep(250000);
   unsigned char data_ar6[] = {0x46,0x00,0xB9,0xA5,0x21,0xFB,0xBD,0x72,0xC3,0x45};
-  send_msg(fd, , data_ar6,10);
+  send_msg(fd, data_ar6,10);
   usleep(250000);
   unsigned char data_ar7[] = {0x42,0x00,0x10,0x00};
-  send_msg(fd, , data_ar7,4);
+  send_msg(fd, data_ar7,4);
   usleep(250000);
   unsigned char data_ar8[] = {0x51,0x00,0x01,0x02,0x11,0x10};
-  send_msg(fd, , data_ar8,6);
+  send_msg(fd, data_ar8,6);
   usleep(250000);
   unsigned char data_ar9[] = {0x44,0x00,0xFF};
-  send_msg(fd, , data_ar9,3);
+  send_msg(fd, data_ar9,3);
   usleep(250000);
   unsigned char data_ar10[] = {0x43,0x00,0x86,0x1F};
-  send_msg(fd, , data_ar10,4);
+  send_msg(fd, data_ar10,4);
   usleep(250000);
   unsigned char data_ar11[] = {0x45,0x00,0x39};
-  send_msg(fd, , data_ar11,3);
+  send_msg(fd, data_ar11,3);
   usleep(250000);
   unsigned char data_ar12[] = {0x4B,0x00};
-  send_msg(fd, , data_ar12,2);
+  send_msg(fd, data_ar12,2);
 }
 
 ///packet transmission
@@ -145,7 +145,7 @@ void send_msg(int fd, unsigned char bd[],unsigned char bd_size){
   buf_size = bd_size + 3;
 
   wlen = write(fd, buf, buf_size);
-  if (wlen != 1) {
+  if (wlen != buf_size) {
     printf("Error from write: %d, %d\n", wlen, errno);
   }
   
@@ -229,7 +229,7 @@ int set_interface_attribs(int fd, int speed)
 
 int serial_open()
 {
-  char *portname = "/dev/ttyACM0";
+  char *portname = "/dev/ttyUSB0";
   int fd;
   fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
   if (fd < 0) {
